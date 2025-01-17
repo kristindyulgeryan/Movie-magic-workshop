e7014b59745a08e54d477f8cf92ab8fc991ef418
Kristin <kristindyulgeryan@abv.bg>
2025-01-17 16:41:59 +0200

    Add main layout

--- src/index.js
+++ src/index.js
@@ -16,6 +16,6 @@ app.use('/static',express.static('src/public'))
 
 app.get('/', (req,res)=>{
 
-    res.render('home', {layout: false})
+    res.render('home')
 })
 app.listen(5000,()=> console.log('Server is listening on http://localhost:5000...'))
