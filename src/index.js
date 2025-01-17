6acf73de98db2d8d102a8c3d42c5dd55171a65d1
Kristin <kristindyulgeryan@abv.bg>
2025-01-17 16:09:16 +0200

    Setup express - static

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ const app = express()
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
-}))
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+app.use(express.static('src/public'))
 
 app.get('/', (req,res)=>{
 
