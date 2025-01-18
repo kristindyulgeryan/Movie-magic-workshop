5fe48180eb3ecfd84dafa6e2292c78a13e20bf8d
Kristin <kristindyulgeryan@abv.bg>
2025-01-18 16:36:56 +0200

    Add dynamic movie page

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,9 +1,10 @@
 import express from "express";
+import movies from "../movies.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { movies });
 });
 router.get("/about", (req, res) => {
   res.render("about");
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 import homeController from "./controllers/home-controller.js";
 
 const routes = Router();
+
 routes.use(homeController);
+
 routes.get("*", (req, res) => {
   res.render("404");
 });
