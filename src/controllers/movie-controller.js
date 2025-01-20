9acd9b5529dee51d322d2b7fd1aaf4ac34d977bb
Kristin <kristindyulgeryan@abv.bg>
2025-01-20 10:09:43 +0200

    Change and add search page

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -3,7 +3,11 @@ import movieService from "../services/movie-service.js";
 
 const movieController = Router();
 
+
+
 movieController.get("/search", (req, res) => {
+ 
+
   res.render("search");
 });
 movieController.get("/create", (req, res) => {
