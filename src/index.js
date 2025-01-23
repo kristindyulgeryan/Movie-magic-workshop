79f1e0146d930319c301d7411b71437def80a630
Kristin <kristindyulgeryan@abv.bg>
2025-01-23 16:38:26 +0200

    Config handlebars to mongoose documents

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ app.engine(
   "hbs",
   handlebars.engine({
     extname: "hbs",
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+   
+    },
     helpers: {
       showRating: showRatingHelper,
     },
